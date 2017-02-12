02d59a8ff1deba96171dce39c164950081a52592
Stefano Pernat <stefano.pernat@gmail.com>
2017-02-12 17:29:34 +0100

    Removed unused import

--- app/src/main/java/com/github/stefanopernat/popmovies/MainActivityFragment.java
+++ app/src/main/java/com/github/stefanopernat/popmovies/MainActivityFragment.java
@@ -1,16 +1,10 @@
 package com.github.stefanopernat.popmovies;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.github.stefanopernat.popmovies.util.Util;
 
 
 public class MainActivityFragment extends Fragment {
